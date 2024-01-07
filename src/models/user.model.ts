d0a187e6d16bb733fd612c8448adbc9de9398795
Toni <abarrantes@sequentiabiotech.com>
2024-01-07 22:15:53 +0100

    Add authentication feature

---
+++ src/models/user.model.ts
@@ -0,0 +1,38 @@
+import mongoose, { Document, Schema } from 'mongoose'
+import bcrypt from 'bcryptjs'
+import { IUser } from '../types/user'
+
+interface IUserModel extends IUser, Document {
+  comparePassword: (password: string) => Promise<boolean>
+}
+
+const UserSchema: Schema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    username: { type: String, required: true },
+    password: { type: String, required: true },
+    role: { type: Number, required: true },
+    internalUser: { type: Boolean, required: true },
+  },
+  {
+    timestamps: true,
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.password
+      },
+    },
+  },
+)
+
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next()
+  const hash = await bcrypt.hash(this.password, 10)
+  this.password = hash
+})
+
+UserSchema.methods.comparePassword = function (password: string): Promise<boolean> {
+  return bcrypt.compare(password, this.password)
+}
+
+const User = mongoose.model<IUserModel>('User', UserSchema)
+export default User
--- src/routes/index.route.ts
+++ src/routes/index.route.ts
@@ -4,6 +4,7 @@ import locationRouter from './location.route'
 import photoRouter from './photo.route'
 import reportRouter from './report.route'
 import userRouter from './user.route'
+import authenticationRouter from './authentication.route'
 
 const router = Router()
 
@@ -12,5 +13,6 @@ router.use('/location', locationRouter)
 router.use('/photo', photoRouter)
 router.use('/report', reportRouter)
 router.use('/user', userRouter)
+router.use('/auth', authenticationRouter)
 
 export default router
