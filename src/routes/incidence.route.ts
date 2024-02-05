c8bc2b9fecbe1a5dd0a31e1fd0ffe234db100874
T0nete <tonbarramon@gmail.com>
2024-02-05 22:41:58 +0100

    define new endpoint

--- src/routes/incidence.route.ts
+++ src/routes/incidence.route.ts
@@ -1,10 +1,10 @@
 import express from 'express'
-import { createNewIncidence, getHallIncidences } from '../controllers/incidence.controller'
+import { createNewIncidence, getAllIncidences } from '../controllers/incidence.controller'
 import { isAdminUser } from '../middlewares/authentication'
 
 const incidenceRouter = express.Router()
 
 incidenceRouter.post('/', createNewIncidence)
-incidenceRouter.get('/', isAdminUser, getHallIncidences)
+incidenceRouter.get('/', isAdminUser, getAllIncidences)
 
 export default incidenceRouter
